96302eb4fdb2f4163dd1429c2d2813c17b998cf3
lhikarul <711hikaru@gmail.com>
2022-07-24 20:22:45 +0800

    chore: Accordion storybook added

--- src/components/Accordion/Accordion.style.tsx
+++ src/components/Accordion/Accordion.style.tsx
@@ -1,33 +1,30 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   margin-bottom: 0.5rem;
   border-radius: 0.5rem;
   border: 1px solid #4c4c57;
-`
+  padding: 0 1rem;
+`;
 
 export const HeaderWrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   height: 82px;
   position: relative;
   cursor: pointer;
-`
+`;
 
 export const PanelAnimation = styled.div<{ maxHeight: number }>`
   max-height: ${(props) => props.maxHeight}px;
   overflow: hidden;
   transition: max-height 0.3s ease-out;
-`
+`;
 export const ArrowSwitch = styled.span<{ isUp: boolean }>`
-  padding-left: 0.5rem;
   min-width: 1.5rem;
   cursor: pointer;
-  position: absolute;
-  right: 1.8125rem;
-  top: 50%;
-  transform: translateY(-50%);
   > svg {
-    transform: rotate(${(props) => (props.isUp ? '180deg' : '0deg')});
+    transform: rotate(${(props) => (props.isUp ? "180deg" : "0deg")});
   }
-`
+`;
