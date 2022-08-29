d1e65dfcc2506616a817a4917c4f015c431917ea
lhikarul <711hikaru@gmail.com>
2022-08-29 21:33:16 +0800

    feat: SimpleVirtualizedList

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import { ThemeProvider } from "@emotion/react";
+import { useEffect, useState } from "react";
+import SimpleVirtualizedList from "./components/SimpleVirtualizedList";
 import { theme } from "./theme";
-import { useAuth } from "./context/auth-context";
+
+const ITEM_COUNT = 500;
 
 function App() {
-  const { login, logout, user } = useAuth();
+  const [items, setItems] = useState<{ index: number; name: string }[]>([]);
+
+  useEffect(() => {
+    setItems(
+      new Array(ITEM_COUNT)
+        .fill(null)
+        .map((_, i) => ({ index: i, name: `Item ${i}` }))
+    );
+  }, []);
   return (
     <ThemeProvider theme={theme.default}>
-      <button
-        onClick={() =>
-          login({
-            username: "Evans",
-            password: "123456",
-            userId: 1,
-          })
-        }
-      >
-        login
-      </button>
-      <div>{JSON.stringify(user)}</div>
+      <SimpleVirtualizedList
+        itemCount={items.length}
+        itemHeight={40}
+        windowHeight={400}
+        renderItem={({ index, style }) => {
+          const i = items[index];
+          return (
+            <div key={i.name} className="item" style={style}>
+              <label>{i.name}</label>
+            </div>
+          );
+        }}
+      />
     </ThemeProvider>
   );
 }
---
+++ src/components/SimpleVirtualizedList/index.tsx
@@ -0,0 +1,75 @@
+import React, {
+  CSSProperties,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+type RenderCallbackParams = {
+  index: number;
+  style: CSSProperties;
+};
+
+interface IProps {
+  itemHeight: number;
+  windowHeight: number;
+  itemCount: number;
+  renderItem: (params: RenderCallbackParams) => ReactElement;
+}
+
+function SimpleVirtualizedList(props: IProps) {
+  const { itemHeight, windowHeight, itemCount, renderItem } = props;
+  const [scrollTop, setScrollTop] = useState(0);
+  const [items, setItems] = useState<ReactElement[]>([]);
+  const innerHeight = useMemo(
+    () => itemCount * itemHeight,
+    [itemCount, itemHeight]
+  );
+
+  const startIndex = Math.floor(scrollTop / itemHeight);
+  const endIndex = Math.min(
+    itemCount - 1,
+    Math.floor((scrollTop + windowHeight) / itemHeight)
+  );
+
+  useEffect(() => {
+    const itemsList = [];
+    for (let i = startIndex; i <= endIndex; i++) {
+      itemsList.push(
+        renderItem({
+          index: i,
+          style: {
+            position: "absolute",
+            top: `${i * itemHeight}px`,
+            width: "100%",
+          },
+        })
+      );
+      setItems([...itemsList]);
+    }
+  }, [endIndex, itemHeight, renderItem, startIndex]);
+
+  const onScroll = useCallback(
+    (e: React.UIEvent<HTMLElement>) => setScrollTop(e.currentTarget.scrollTop),
+    []
+  );
+
+  return (
+    <div
+      className="scroll"
+      style={{ overflowY: "scroll", height: `${windowHeight}px` }}
+      onScroll={onScroll}
+    >
+      <div
+        className="inner"
+        style={{ position: "relative", height: `${innerHeight}px` }}
+      >
+        {items}
+      </div>
+    </div>
+  );
+}
+
+export default SimpleVirtualizedList;
